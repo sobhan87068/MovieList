0bee690e70e352a83a6e24dab8af2ea5c593c3dd
sobhan <sobhan.aryannamazi@gmail.com>
2024-02-12 11:42:10 +0330

    Fixed status bar icon colors

--- app/src/main/java/com/example/movielist/ui/theme/Theme.kt
+++ app/src/main/java/com/example/movielist/ui/theme/Theme.kt
@@ -65,7 +65,7 @@ fun MovieListTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
